3683e58b017eb03938092890a8345ba15fb8d167
wolfspider <jesse.alexander.bennnett@gmail.com>
2022-01-14 01:23:51 -0500

    finish up crud

--- src/main.rs
+++ src/main.rs
@@ -123,6 +123,24 @@ async fn main() {
         delete_post(my_id, msg, &users, dbinstance);
     warp::reply()});
 
+    let post_commit = 
+     warp::path("commit")
+    .and(warp::post())
+    .and(warp::path::param::<usize>())
+    .and(warp::body::content_length_limit(500))
+    .and(
+        warp::body::bytes().and_then(|body: bytes::Bytes| async move {
+            std::str::from_utf8(&body)
+                .map(String::from)
+                .map_err(|_e| warp::reject::custom(NotUtf8))
+            }),
+        )
+    .and(users.clone())
+    .and(dbinstance.clone())
+    .map(|my_id, msg, users, dbinstance| { 
+        get_posts_commit(my_id, msg, &users, dbinstance);
+    warp::reply()});
+
     // GET /chat -> messages stream
     let chat_recv = warp::path("chat").and(warp::get()).and(users).map(|users| {
         // reply using server-sent events
@@ -142,7 +160,8 @@ async fn main() {
     .or(vid)
     .or(get_posts)
     .or(posts_status)
-    .or(post_delete);
+    .or(post_delete)
+    .or(post_commit);
 
     warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 
@@ -212,6 +231,32 @@ fn delete_post(my_id: usize, msg: String, users: &Users, dbinstance: Arc<fdb::Da
     let delidus = usize::try_from(delid).unwrap();
     
     let vecstr = futures::executor::block_on(models::fdb_model::delete_post_query(&dbinstance, delidus));
+    
+    for fdbstr in vecstr {
+        let compstr = format!("{} ,", &fdbstr);
+        new_msg.push_str(&compstr);
+    }
+
+    // New message from this user, send it to everyone else (except same uid)...
+    //
+    // We use `retain` instead of a for loop so that we can reap any user that
+    // appears to have disconnected.
+    users.lock().unwrap().retain(|uid, tx| {
+        /*
+        if my_id == *uid {
+            // don't send to same user, but do retain
+            true
+        } else {*/
+            // If not `is_ok`, the SSE stream is gone, and so don't retain
+            tx.send(Message::Reply(new_msg.clone())).is_ok()
+        //}
+    });
+}
+
+fn get_posts_commit(my_id: usize, msg: String, users: &Users, dbinstance: Arc<fdb::Database>)  {
+    let mut new_msg = format!("User::User#{}: {},", my_id, msg);
+    
+    let vecstr = futures::executor::block_on(models::fdb_model::commit_posts_query(&dbinstance));
     
     for fdbstr in vecstr {
         let compstr = format!("{} ,", &fdbstr);
